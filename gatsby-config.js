663dbc257fc69d679f053c65ef4ea3b16aaf13a7
Dawit Bezu <56790067+DawitBezu@users.noreply.github.com>
2020-10-26 21:17:27 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    description: "Personal page of Dawit",
+    locale: "en",
+    title: "Dawit Bezu | JS Developer",
+  },
   plugins: [
     {
       resolve: "@wkocjan/gatsby-theme-intro",
